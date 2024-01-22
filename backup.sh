616f690069ade75fd4879ceda9c08d0315fb5aee
kesien <balazsnobik@gmail.com>
2024-01-22 11:53:30 +0100

    recovered

--- backup.sh
+++ backup.sh
@@ -1,57 +1,56 @@
 #!/bin/bash
-
+log_file="$(pwd)/backup_log.txt"
+echo "$(date +'%Y-%m-%d %H:%M:%S') Backup script staring ..." >> $log_file
 dotenv_file="./.env"
-source "$dotenv_file" || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error: Unable to load environment variables from $dotenv_file"; exit 1; }
-backup_name=$NAME
+source "$dotenv_file" || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error: Unable to load environment variables from $dotenv_file" >> $log_file; exit 1; }
 
-cd $BACKUP_DIR
-
+cd $BACKUP_DESTINATION
 # Create a tar file with the specified naming convention
-tar_filename="backup_${backup_name}_$(date +'%Y%m%d').tar"
-(tar -czvf "$tar_filename" "$BACKUP_DIR" >> backup_log.txt 2>&1) || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error creating tar file." >> backup_log.txt; exit 1; }
+tar_filename="backup_${NAME}_$(date +'%Y%m%d').tar"
+echo "$(date +'%Y-%m-%d %H:%M:%S') Creating tar file: $tar_filename" >> $log_file
+(tar -czvf "$tar_filename" -C "$APPLICATION_PATH" . >> /dev/null 2>&1) || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error creating tar file." >> $log_file; exit 1; }
 
 # Create a mysqldump with the same naming convention
-mysql_dump_filename="backup_${MYSQL_DATABASE}_$(date +'%Y%m%d').sql"
-(mysqldump -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" > "$mysql_dump_filename" >> backup_log.txt 2>&1) || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error creating mysqldump file." >> backup_log.txt; exit 1; }
+mysql_dump_filename="backup_${NAME}_$(date +'%Y%m%d').sql"
+echo "$(date +'%Y-%m-%d %H:%M:%S') Creating mysql dump file: $mysql_dump_filename" >> $log_file
+(mysqldump -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" > "$mysql_dump_filename") || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error creating mysqldump file." >> $log_file; exit 1; }
 
 # Zip the tar and SQL files into the same zip
-zip_name="backup_$(date +'%Y%m%d').zip"
-zip -m "$zip_name" "$tar_filename" "$mysql_dump_filename" >> backup_log.txt 2>&1 || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error zipping files." >> backup_log.txt; exit 1; }
+zip_name="backup_${NAME}_$(date +'%Y%m%d').zip"
+echo "$(date +'%Y-%m-%d %H:%M:%S') Creating $zip_name file from $tar_filename and $mysql_dump_filename" >> $log_file
+(zip -mq "$zip_name" "$tar_filename" "$mysql_dump_filename") || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error zipping files." >> $log_file; exit 1; }
 
 # Copy files to another server using scp
-(scp -o StrictHostKeyChecking=no "$zip_name" "$REMOTE_USER"@"$REMOTE_ADDRESS":"$REMOTE_DESTINATION" >> backup_log.txt 2>&1) || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error copying files to remote server." >> backup_log.txt; exit 1; }
-if [ $? -ne 0 ]; then
-    echo "Error copying files to remote server." >> backup_log.txt
-    exit 1
-fi
+echo "$(date +'%Y-%m-%d %H:%M:%S') Copying files to remote host ..." >> $log_file
+(scp -o StrictHostKeyChecking=no "$zip_name" "$REMOTE_USER"@"$REMOTE_ADDRESS":"$REMOTE_DESTINATION" >> $log_file 2>&1) || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error copying files to remote server." >> $log_file; exit 1; }
 
 # Run remote script
 zip_path=$REMOTE_DESTINATION/$zip_name
-ssh -o StrictHostKeyChecking=no "$REMOTE_USER"@"$REMOTE_ADDRESS" "bash $zip_path $REMOTE_MYSQL_USER $REMOTE_MYSQL_PASSWORD $REMOTE_MYSQL_DATABASE $REMOTE_RESTORE_DESTIONATION" >> backup_log.txt 2>&1
+echo "$(date +'%Y-%m-%d %H:%M:%S') Start running remote script ..." >> $log_file
+ssh -o StrictHostKeyChecking=no "$REMOTE_ADDRESS" "bash $REMOTE_SCRIPT_PATH \"$zip_path\" \"$REMOTE_MYSQL_USER\" \"$REMOTE_MYSQL_PASSWORD\" \"$REMOTE_MYSQL_DATABASE\" \"$REMOTE_RESTORE_DESTINATION\"" >> $log_file 2>&1
 if [ $? -ne 0 ]; then
-    echo "Error running script on remote server." >> backup_log.txt
+    echo "Error running script on remote server." >> $log_file
     exit 1
 fi
+echo "$(date +'%Y-%m-%d %H:%M:%S') Remote script finished" >> $log_file
 
-# Delete backups from the previous month, retaining the earliest backup for each day
+# Delete backups from the previous month, retaining the latest backup for each month
+echo "$(date +'%Y-%m-%d %H:%M:%S') Deleting files from the previous month, retaining the latest backup for each month" >> $log_file
+count=0
 previous_month=$(date -d 'last month' +'%Y%m')
-IFS=$'\n' read -r -a backups_to_delete <<< "$(find "$BACKUP_DIR" -type f -name "backup_*_${previous_month}.zip")"
-declare -A earliest_backup_per_day
+mapfile -t backups_to_delete < <(find "$BACKUP_DESTINATION" -type f -name "backup_${NAME}_${previous_month}*.zip")
+latest_backup=""
 for backup in "${backups_to_delete[@]}"; do
-    day=$(basename "$backup" | sed 's/.*backup_\(.*\)\.zip/\1/')
-    if [[ ! ${earliest_backup_per_day[$day]} || "$backup" < "${earliest_backup_per_day[$day]}" ]]; then
-        earliest_backup_per_day[$day]="$backup"
+    if [[ -z "$latest_backup" || "$backup" > "$latest_backup" ]]; then
+        latest_backup="$backup"
     fi
 done
 for backup in "${backups_to_delete[@]}"; do
-    day=$(basename "$backup" | sed 's/.*backup_\(.*\)\.zip/\1/')
-    if [[ "$backup" != "${earliest_backup_per_day[$day]}" ]]; then
+    if [[ "$backup" != "$latest_backup" ]]; then
         rm "$backup"
+        ((count=count+1))
     fi
 done
-unset IFS>> backup_log.txt 2>&1
+echo "$(date +'%Y-%m-%d %H:%M:%S') Deleted $count files from the previous month." >> $log_file
 
-# Delete local tar and mysql files
-(rm "$tar_filename" "$mysql_dump_filename" >> backup_log.txt 2>&1) || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error deleting local backup files." >> backup_log.txt; exit 1; }
-
-echo "Backup and transfer completed successfully." >> backup_log.txt
+echo "$(date +'%Y-%m-%d %H:%M:%S') Backup and transfer completed successfully." >> $log_file
--- restore.sh
+++ restore.sh
@@ -2,7 +2,7 @@
 
 # Check if the required arguments are provided
 if [ "$#" -ne 5 ]; then
-    echo "$(date +'%Y-%m-%d %H:%M:%S') Usage: $0 <zip_file> <mysql_user> <mysql_password> <mysql_database> <destionation_directory>" >> backup_log.txt
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Usage: $0 <zip_file> <mysql_user> <mysql_password> <mysql_database> <destionation_directory>"
     exit 1
 fi
 
@@ -19,24 +19,26 @@ zip_name=$(basename $zip_file)
 cd "$zip_directory"
 
 # Unzip the provided zip file
-unzip -q "$zip_file" >> backup_log.txt 2>&1 || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error unzipping the file." >> backup_log.txt; exit 1; }
+echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Unzipping $zip_name..."
+unzip -qo "$zip_file"|| { echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Error unzipping the file."; exit 1; }
 
 # Extract the tar file name from the zip
 backup_filename=$(unzip -l "$zip_file" | grep ".tar" | awk '{ print $4 }')
 
 # Untar the contents of the tar file
-tar -xzf "$backup_filename" backup_log.txt 2>&1 || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error untarring the file." >> backup_log.txt; exit 1; }
-
-# Copy the extracted files to the specified directory, overwriting existing files
-cp -r "$backup_filename" "$destination_directory" || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error copying files to destination directory." >> backup_log.txt; exit 1; }
+echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Extracting the contents of $backup_filename to $destination_directory"
+(tar -xzf "$backup_filename" -C $destination_directory >> /dev/null 2>&1) || { echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Error extracting the file $backup_filename"; exit 1; }
 
 # Import the MySQL file into the specified database
-mysql -u "$mysql_user" -p"$mysql_password" -e "source $destination_path/$backup_filename.sql" "$mysql_database" || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error importing MySQL file." >> backup_log.txt; exit 1; }
+echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Importing ${backup_filename%.tar}.sql into database: $mysql_database"
+mysql -u "$mysql_user" -p"$mysql_password" "$mysql_database" < "${backup_filename%.tar}.sql" || { echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Error importing MySQL file."; exit 1; }
 
 # Delete older zip files after completion, retaining only the most recent one
+echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Deleting old zip files keeping only the most recent one"
 find ./ -maxdepth 1 -type f -name "backup_*.zip" -not -name "$zip_name" -exec rm {} +
 
 # Delete all files after completion
-rm -rf "$backup_filename.tar" "$backup_filename" "$backup_filename.sql" || { echo "$(date +'%Y-%m-%d %H:%M:%S') Error deleting files." >> backup_log.txt; exit 1; }
+echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Deleting temporary files"
+rm "$backup_filename" "${backup_filename%.tar}.sql" || { echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] Error deleting files."; exit 1; }
 
-echo "$(date +'%Y-%m-%d %H:%M:%S') Backup restoration completed successfully." >> backup_log.txt
+echo "$(date +'%Y-%m-%d %H:%M:%S') [REMOTE] All tasks completed successfully"
